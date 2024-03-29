1907048b74ba0da46b5cdf26e00a8e3441e50e17
Виктория Голубова <v.golubova@denexy.com>
2024-03-29 19:52:10 +0400

    Added key onParamEdit for interface Props.

--- src/types/Props.ts
+++ src/types/Props.ts
@@ -1,7 +1,9 @@
 import Model from "./Model";
 import Param from "./Param";
+import {FormEvent} from "react";
 
 export default interface Props {
     params: Param[],
     model: Model,
+    onParamEdit: (e: FormEvent, id: number) => void,
 }
