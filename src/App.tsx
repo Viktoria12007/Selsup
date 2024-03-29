347971f59121f64023f0345f373916983209b293
Виктория Голубова <v.golubova@denexy.com>
2024-03-29 18:29:33 +0400

    The operating logic of the app component has been changed.

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,37 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import {ParamEditor} from "./components/ParamEditor";
+import Param from "./types/Param";
+import Model from "./types/Model";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const params: Param[] =[
+      { id: 1, name: 'Назначение' },
+      { id: 2, name: 'Длина' },
+      { id: 3, name: 'Размер' },
+      { id: 4, name: 'Цвет' },
+    ]
+  const [model, setModel] = useState<Model>({
+      paramValues: [
+          { paramId: 1, value: 'повседневное' },
+          { paramId: 2, value: 'макси' },
+          { paramId: 3, value: 44 },
+          { paramId: 4, value: ['красный', 'синий', 'зелёный', 'жёлтый'] },
+      ]
+  })
+
+  function handleParamEdit(e, id) {
+    setModel({ paramValues: model.paramValues.map((paramValue) => {
+        if (paramValue.paramId === id) {
+          return { ...paramValue, value: e.target.value }
+        }
+        return paramValue
+      })})
+    console.log(model);
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <ParamEditor params={params} model={model} handleParamEdit={handleParamEdit} />
   )
 }
 
