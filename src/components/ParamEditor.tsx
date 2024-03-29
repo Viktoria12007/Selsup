71398fae5672fbc5443c0fd0f0c8fa10dda11e1e
Виктория Голубова <v.golubova@denexy.com>
2024-03-29 19:53:47 +0400

    ParamEditor.tsx is rewritten in class style.

--- src/components/ParamEditor.tsx
+++ src/components/ParamEditor.tsx
@@ -1,30 +1,45 @@
-import {FC} from "react";
-import Props from "../types/Props";
+import {Component, FormEvent} from "react";
 import '../App.css';
+import Props from "../types/Props";
+import Model from "../types/Model";
 
-export const ParamEditor: FC<Props> = ({ params, model, handleParamEdit }) => {
-    return (
-        <div className="container">
-            {
-                params.map((param) => {
-                    const paramValue = model.paramValues.find((paramValue) => paramValue.paramId === param.id).value
-                    return (
-                        typeof paramValue === 'string' &&
-                        <label
-                            key={param.id}
-                            className="label"
-                        >
-                            {param.name}
-                            <input
-                                value={paramValue}
-                                onInput={(e) => handleParamEdit(e, param.id)}
-                                className="input"
-                                type="text"
-                            />
-                        </label>
-                    )
-                })
-            }
-        </div>
-    )
+class ParamEditor extends Component<Props, any> {
+    constructor(props) {
+        super(props);
+        this.handleParamEdit = this.handleParamEdit.bind(this);
+    }
+    public getModel(): Model {
+        return this.props.model;
+    }
+    handleParamEdit(e: FormEvent, id: number) {
+        this.props.onParamEdit(e, id);
+        console.log(this.getModel())
+    }
+    render() {
+        return (
+            <div className="container">
+                {
+                    this.props.params.map((param) => {
+                        const paramValue = this.props.model.paramValues.find((paramValue) => paramValue.paramId === param.id).value
+                        return (
+                            typeof paramValue === 'string' &&
+                            <label
+                                key={param.id}
+                                className="label"
+                            >
+                                {param.name}
+                                <input
+                                    value={paramValue}
+                                    onInput={(e) => this.handleParamEdit(e, param.id)}
+                                    className="input"
+                                    type="text"
+                                />
+                            </label>
+                        )
+                    })
+                }
+            </div>
+        )
+    }
 }
+export default ParamEditor
